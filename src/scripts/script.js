50e2abdcc4a71751342364ff462560b37a5292e8
Leozende <leonardo-zende@hotmail.com>
2023-12-29 08:49:16 -0300

    feat: animation and disable others

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,41 +1,58 @@
-const questions = document.querySelectorAll(".faq-questions>li")
+const questions = document.querySelectorAll(".faq-questions>ul>li");
 
 questions.forEach((event, i) => {
-    const question = event.querySelector(".faq-question")
-    question.addEventListener('click',()=> activeContent(question))
-    question.addEventListener('keydown',(e) => {
-        if(e.key === 'ArrowUp'){
-            if(i !== 0){
-                questions[(i-1)].querySelector(".faq-question .faq-question__title").focus()
-            } else{
-                questions[3].querySelector(".faq-question .faq-question__title").focus()
-            }
-        }else if(e.key === 'ArrowDown'){
-            if(i !== 3){
-                questions[(i+1)].querySelector(".faq-question .faq-question__title").focus()
-            } else{
-                questions[0].querySelector(".faq-question .faq-question__title").focus()
-            }
-        }else if(e.key === 'Enter'){
-            activeContent(question)
-        }else if(e.key === 'Tab' ){
-            if(i === 3){
-                questions[0].querySelector(".faq-question .faq-question__title").focus()
-            }
-        }
-    })
-})
+  const question = event.querySelector(".faq-question");
+  question.addEventListener("click", () => activeContent(question));
+  question.addEventListener("keydown", (e) => handleKeyDown(e, questions, i));
+});
 
-function activeContent(question){
-    const questionContent = question.querySelector("p")
-    const questionImg = question.querySelector("img")
+function activeContent(question) {
+  const questionContent = question.querySelector("p");
+  const questionImg = question.querySelector("img");
 
-    if(questionContent.classList.contains("active")){
-        questionContent.classList.remove("active")
-        questionImg.src = "./assets/images/icon-minus.svg"
-        
-    } else {
-        questionContent.classList.add("active")
-        questionImg.src = "./assets/images/icon-plus.svg"
+  questions.forEach((event) => {
+    const otherQuestion = event.querySelector(".faq-question");
+    if (otherQuestion !== question) {
+      disableContent(otherQuestion);
     }
+  });
+
+  questionContent.classList.toggle("active");
+  const activeClass = questionContent.classList.contains("active");
+  questionImg.src = activeClass
+    ? "./assets/images/icon-minus.svg"
+    : "./assets/images/icon-plus.svg";
+}
+
+function disableContent(otherQuestion) {
+  const otherQuestionContent = otherQuestion.querySelector("p");
+  const otherQuestionImg = otherQuestion.querySelector("img");
+  otherQuestionContent.classList.remove("active");
+  otherQuestionImg.src = "./assets/images/icon-plus.svg";
+}
+
+function handleKeyDown(e, questions, index) {
+  const lastIndex = questions.length - 1;
+
+  switch (e.key) {
+    case "ArrowUp":
+      focusQuestionTitle(index !== 0 ? index - 1 : lastIndex);
+      break;
+    case "ArrowDown":
+      focusQuestionTitle(index !== 0 ? index + 1 : 0);
+      break;
+    case "Enter":
+      activeContent(questions[index]);
+      break;
+    case "Tab":
+      if (index === lastIndex) focusQuestionTitle(0);
+      break;
+
+    default:
+      break;
+  }
+}
+
+function focusQuestionTitle(index) {
+  questions[index].querySelector(".faq-question-title").focus();
 }
